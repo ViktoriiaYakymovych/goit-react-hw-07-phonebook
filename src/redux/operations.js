981ad804f42b513b59640ce2d60099038c1c0820
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-12-06 14:02:21 +0200

    a

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,28 +1,28 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://65688d5a9927836bd975093c.mockapi.io';
+axios.defaults.baseURL = 'https://65688d5a9927836bd975093c.mockapi.io';
 
-// export const fetchContacts = createAsyncThunk(
-//   'contacts/fetchAll',
-//   async (_, thunkAPI) => {
-//     try {
-//       const resp = await axios.get('/contacts');
-//       return resp.data;
-//     } catch (err) {
-//       return thunkAPI.rejectWithValue(err.message);
-//     }
-//   }
-// );
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await axios.get('/contacts');
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
 
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async (contact, thunkAPI) => {
-//     try {
-//       const resp = await axios.post('/contacts', { contact });
-//       return resp.data;
-//     } catch (err) {
-//       return thunkAPI.rejectWithValue(err.message);
-//     }
-//   }
-// );
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const resp = await axios.post('/contacts', { contact });
+      return resp.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
